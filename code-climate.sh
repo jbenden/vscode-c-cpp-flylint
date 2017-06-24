e8e94e40dc5b7c55ba389cf38da7b984ef692c1f
Joseph Benden <joe@benden.us>
2017-06-24 10:00:40 -0700

    Added Code Climate supporting files

---
+++ code-climate.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+sudo env CONTAINER_TIMEOUT_SECONDS=1800 docker run \
+  --interactive --tty --rm \
+  --env CODECLIMATE_CODE="$PWD" \
+  --volume "$PWD":/code \
+  --volume /var/run/docker.sock:/var/run/docker.sock \
+  --volume /tmp/cc:/tmp/cc \
+  codeclimate/codeclimate "$@"
