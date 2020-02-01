be2d0425e3ce075125a3d0e2a05ddf8457e6a36a
TSonono <tofiksonono@msn.com>
2020-02-01 20:38:05 +0100

    Added tests for misra addon

--- server/src/linters/tests/cppcheck.test.ts
+++ server/src/linters/tests/cppcheck.test.ts
@@ -123,4 +123,59 @@ class CppCheckTests {
         result.should.have.property('code', "unusedStructMember");
         expect(result['message']).to.match(/^struct member \'Anonymous5::name_space\' is never used\./);
     }
+
+    @test("Should handle output from misra addon")
+    handleMisra() {
+        let test = [
+            'Defines: CURRENT_DEVICE_VERSION=1;BIG_VERSION=1;LITTLE_VERSION=1;CURRENT_DEVICE_GROUP=1;CURRENT_DEVICE_SLEEP_TYPE=1;CURRENT_ABILITY_1BYTE=1;CURRENT_ABILITY_2BYTE=1;CURRENT_ABILITY_3BYTE=1;CURRENT_ABILITY_4BYTE=1;CLASS=1;CLASS_B=1;CLASS_C=1;IF_UD_RELAY=1;PRIu32="u";PRIx32="x";PRIX32="X";PRIXX32="X";NETSTACK_CONF_WITH_IPV6=1',
+            'Includes: -I/Users/username/Documents/Unwired/contiki_ud_ng/ -I/Users/username/Documents/Unwired/contiki_ud_ng/lib/ -I/Users/username/Documents/Unwired/contiki_ud_ng/core/ -I/Users/username/Documents/Unwired/contiki_ud_ng/core/dev/ -I/Users/username/Documents/Unwired/contiki_ud_ng/core/sys/ -I/Users/username/Documents/Unwired/contiki_ud_ng/core/net/ip/ -I/Users/username/Documents/Unwired/contiki_ud_ng/core/net/ipv6/ -I/Users/username/Documents/Unwired/contiki_ud_ng/unwired/ -I/Users/username/Documents/Unwired/contiki_ud_ng/unwired/asuno-light/ -I/Users/username/Documents/Unwired/contiki_ud_ng/unwired/smarthome/ -I/Users/username/Documents/Unwired/contiki_ud_ng/platform/unwired/ -I/Users/username/Documents/Unwired/contiki_ud_ng/platform/unwired/common/ -I/Users/username/Documents/Unwired/contiki_ud_ng/platform/unwired/udboards/ -I/Users/username/Documents/Unwired/contiki_ud_ng/platform/unwired/udboards/cc26xx/ -I/Users/username/Documents/Unwired/contiki_ud_ng/platform/unwired/udboards/cc13xx/ -I/Users/username/Documents/Unwired/contiki_ud_ng/cpu/cc26xx-cc13xx/ -I/Users/username/Documents/Unwired/contiki_ud_ng/cpu/cc26xx-cc13xx/dev/ -I/Users/username/Documents/Unwired/contiki_ud_ng/cpu/cc26xx-cc13xx/lib/cc26xxware/driverlib/ -I/Users/username/Documents/Unwired/contiki_ud_ng/cpu/cc26xx-cc13xx/lib/cc13xxware/driverlib/ -I/Users/username/Documents/Unwired/contiki_ud_ng/cpu/cc26xx-cc13xx/lib/cc13xxware/ -I/Users/username/Documents/Unwired/contiki_ud_ng/cpu/cc26xx-cc13xx/lib/cc26xxware/ -I/Users/username/Documents/Unwired/contiki_ud_ng/cpu/cc26xx-cc13xx/lib/cc13xxware/inc/ -I/Users/username/Documents/Unwired/contiki_ud_ng/cpu/cc26xx-cc13xx/lib/cc26xxware/inc/ -I/Users/username/Documents/Unwired/contiki_ud_ng/apps/serial-shell/ -I/Users/username/Documents/Unwired/contiki_ud_ng/apps/shell/ -I/usr/local/lib/gcc/arm-none-eabi/6.2.1/include/',
+            'Platform:Native',
+            `"Checking flist.c ..."`,
+            `"flist.c  9  style misra-c2012-10.4: misra violation (use --rule-texts=<file> to get proper output)"`,
+            `"flist.c  11  style misra-c2012-17.7: misra violation (use --rule-texts=<file> to get proper output)"`,
+            `"flist.c  20  style misra-c2012-18.8: misra violation (use --rule-texts=<file> to get proper output)"`,
+            `"flist.c  1  style misra-c2012-21.6: misra violation (use --rule-texts=<file> to get proper output)"`,    
+            `"    information missingIncludeSystem: Cppcheck cannot find all the include files (use --check-config for details)"`,
+            `"    information missingInclude: Cppcheck cannot find all the include files (use --check-config for details)"`,
+        ];
+        let actual = this.linter['parseLines'](test);
+
+        actual.should.have.length(4);
+
+        let result = actual.pop();
+
+        result.should.have.property('fileName', 'flist.c');
+        result.should.have.property('line', 1 - 1);
+        result.should.have.property('column', 0);
+        result.should.have.property('severity', 'Information');
+        result.should.have.property('code', "misra-c2012-21.6");
+        expect(result['message']).to.equal('misra violation (use --rule-texts=<file> to get proper output)');
+
+        result = actual.pop();
+
+        result.should.have.property('fileName', 'flist.c');
+        result.should.have.property('line', 20 - 1);
+        result.should.have.property('column', 0);
+        result.should.have.property('severity', 'Information');
+        result.should.have.property('code', "misra-c2012-18.8");
+        expect(result['message']).to.equal('misra violation (use --rule-texts=<file> to get proper output)');
+
+        result = actual.pop();
+
+        result.should.have.property('fileName', 'flist.c');
+        result.should.have.property('line', 11 - 1);
+        result.should.have.property('column', 0);
+        result.should.have.property('severity', 'Information');
+        result.should.have.property('code', "misra-c2012-17.7");
+        expect(result['message']).to.equal('misra violation (use --rule-texts=<file> to get proper output)');
+
+        result = actual.pop();
+
+        result.should.have.property('fileName', 'flist.c');
+        result.should.have.property('line', 9 - 1);
+        result.should.have.property('column', 0);
+        result.should.have.property('severity', 'Information');
+        result.should.have.property('code', "misra-c2012-10.4");
+        expect(result['message']).to.equal('misra violation (use --rule-texts=<file> to get proper output)');
+    }
 }
