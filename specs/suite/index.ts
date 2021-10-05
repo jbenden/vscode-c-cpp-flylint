7282afa0fecf81ce871537d1e043d6fdc975049d
Joseph Benden <joe@benden.us>
2021-10-05 15:33:54 -0700

    feat: Add e2e mocha-based testing
    Signed-off-by: Joseph Benden <joe@benden.us>

---
+++ specs/suite/index.ts
@@ -0,0 +1,33 @@
+import * as path from 'path';
+import * as Mocha from 'mocha';
+import * as glob from 'fast-glob';
+
+export function run(): Promise<void> {
+  // Create the mocha test
+  const mocha = new Mocha({
+    ui: 'tdd',
+    color: true
+  });
+
+  const testsRoot = path.resolve(__dirname, '..');
+
+  return new Promise(async (c, e) => {
+      const files = await glob('**/**.test.js', { cwd: testsRoot })!;
+
+      // Add files to the test suite
+      files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
+
+      try {
+        // Run the mocha test
+        mocha.run(failures => {
+          if (failures > 0) {
+            e(new Error(`${failures} tests failed.`));
+          } else {
+            c();
+          }
+        });
+      } catch (err) {
+        e(err);
+      }
+    });
+}
